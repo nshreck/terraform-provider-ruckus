de8c9339490324690e6472658e336713b58387ca
Nathan Shreck <shreckn@msoe.edu>
2026-03-13 13:49:45 -0500

    Go go gadget: Linting

--- main.go
+++ main.go
@@ -2,13 +2,21 @@ package main
 
 import (
 	"context"
+	"log"
 
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 	"github.com/shreckn/terraform-provider-ruckus/internal/provider"
 )
 
 func main() {
-	providerserver.Serve(context.Background(), provider.New, providerserver.ServeOpts{
-		Address: "registry.terraform.io/shreckn/ruckus",
-	})
+	if err := providerserver.Serve(
+		context.Background(),
+		provider.New,
+		providerserver.ServeOpts{
+			Address: "registry.terraform.io/shreckn/ruckus",
+		},
+	); err != nil {
+		// Required for errcheck; a crash is appropriate here
+		log.Fatalf("Error running Terraform provider server: %v", err)
+	}
 }

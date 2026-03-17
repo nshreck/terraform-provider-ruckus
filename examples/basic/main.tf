76cb0b7b6789c8088aff5fae4b05cd53d9e5da74
Nathan Shreck <shreckn@msoe.edu>
2026-03-17 13:01:00 -0500

    Update terraform stuffs

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -15,14 +15,14 @@ provider "ruckus" {
   insecure_skip_verify = var.insecure
 }
 
-data "ruckus_zone" "hq" {
+data "ruckus_zone" "zone" {
   name = var.zone
 }
 
-resource "ruckus_wlan" "corp" {
+resource "ruckus_wlan" "wlan" {
   zone_id     = data.ruckus_zone.hq.id
   name        = var.ssid
-  ssid        = "${var.ssid}WLAN"
+  ssid        = var.ssid
 
   security {
     mode        = "wpa2_psk"
